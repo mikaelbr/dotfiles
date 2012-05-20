9d4e86765d24021ab30a19162b91f51f7870ee82
Mikael Brevik <mikaelb@mikaelb.net>
2012-05-20 16:32:48 +0200

    Initial commit. Added zsh config with aliases

---
+++ README.md
@@ -0,0 +1,4 @@
+# dotfiles
+My dotfiles used for importing to new systems and backup
+
+Work in progress: Exporting all dotfiles and making build script
---
+++ zsh/aliases.sh
@@ -0,0 +1,14 @@
+# Example aliases
+alias zshconfig="subl ~/.zshrc"
+alias ohmyzsh="subl ~/.oh-my-zsh"
+
+alias webots="/Applications/Webots/webots.app/Contents/MacOS/webots"
+alias cdexp="cd ~/Sites/experiments"
+
+alias gb="git branch"
+alias gba="git branch -a"
+alias gc="git commit -v"
+alias gd="git diff | subl"
+alias gl="git pull"
+alias gp="git push"
+alias gst="git status"
